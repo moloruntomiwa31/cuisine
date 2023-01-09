70776970e89864687707cc43abc5718da386502c
Aderibigbe Michael Oloruntomiwa <107891405+moloruntomiwa31@users.noreply.github.com>
2023-01-09 21:27:46 +0100

    Added files.

---
+++ app.js
@@ -0,0 +1,25 @@
+const getElement = (selector) => {
+    const element = document.querySelector(selector)
+    if (element) return element
+    throw Error (
+        'Check for $(selector)'
+    )  
+}
+
+const links = getElement(".nav-links")
+const navBtnDOM = getElement(".nav-btn")
+
+navBtnDOM.addEventListener("click", () => {
+    links.classList.toggle("show-links")
+})
+
+AOS.init({
+    offset: 200,
+    delay: 100,
+    duration: 600,
+    easing: "ease",
+    once: false,
+    mirror: false,
+    anchorPlacement: "top bottom"
+})
+
